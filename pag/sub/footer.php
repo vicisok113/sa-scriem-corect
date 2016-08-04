96fffc272deeced3dc76e9255ecd1c7aed4321fe
vicisok113 <tudosa.victor@yahoo.com>
2016-08-04 16:45:30 +0300

    👁 Init view includes

---
+++ pag/sub/footer.php
@@ -0,0 +1,3 @@
+<script src="src/js/libs.js"></script>
+
+<script src="src/js/scrips.js"></script>
---
+++ pag/sub/head.php
@@ -0,0 +1,3 @@
+<link rel="stylesheet" type="text/css" href="src/css/libs.css">
+
+<link rel="stylesheet" type="text/css" href="src/css/style.css">
