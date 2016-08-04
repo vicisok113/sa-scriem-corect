e52e5f67e8344683689882f34627f62da268c434
artur99 <david1989mail@yahoo.com>
2016-08-04 20:06:54 +0300

    ⚓ Linkuri absolute

--- lib/config.php
+++ lib/config.php
@@ -1,9 +1,8 @@
 <?php
 
 $config["db.host"] = "localhost";
-
 $config["db.user"] = "root";
-
 $config["db.pass"] = "";
-
 $config["db.name"] = "open2016";
+
+$config["url"] = "http://localhost/sa-scriem-corect/";
--- pag/sub/head.php
+++ pag/sub/head.php
@@ -1,3 +1,3 @@
+<base href="<?php echo $config["url"];?>">
 <link rel="stylesheet" type="text/css" href="src/css/libs.css">
-
 <link rel="stylesheet" type="text/css" href="src/css/style.css">
