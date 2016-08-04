13e5733e8be3ceede10af8e76ba6d66a8699a1ff
artur99 <david1989mail@yahoo.com>
2016-08-05 02:13:51 +0300

    Primele pregătiri audio

--- src/js/scrips.js
+++ src/js/scrips.js
@@ -1,3 +1,24 @@
 $(document).ready(function(){
     $('select').material_select();
 })
+
+player = {
+    rulare: 0,
+    pregateste: function(nume){
+        this.audio = new Audio('src/audio/bine_ai_venit.mp3');
+    },
+    play: function(cb){
+        this.rulare = 1;
+        this.audio.play();
+        this.audio.onended = function(){    
+            player.rulare = 0;
+            player.audio = null;
+            if(typeof cb == 'function') cb();
+        };
+    },
+    stop: function(){
+        this.audio.pause();
+        this.audio = null;
+        this.rulare = 0;
+    }
+}
